aed0b8a44f1380469487fc6ebbe82b3ac68db22a
Sebastien34 <sebastienwendling@iteeweb.fr>
2024-01-06 15:58:57 +0100

    modif

--- src/pages/components/cardDev.tsx
+++ src/pages/components/cardDev.tsx
@@ -5,22 +5,24 @@ import { Card } from "flowbite-react";
 
 export const CardDev = () => {
   return (
-    <Card className="Card rounded-lg border-cyan-500 flex flex-col justify-center items-center max-w max-h">
-      <Image
-        src="/profile.jpg"
-        width={150}
-        height={150}
-        alt="profileSebastien"
-        className="rounded-lg mx-auto mt-5 mb-10"
-      />
-      <section className="text-center">
-        <h1 className=" text-white text-6xl pb-4 mt-5 mb-10">
-          Sébastien Wendling
-        </h1>
-        <h3 className="text-white text-3xl mb-10">
-          Développeur web React/WordPress.
-        </h3>
-      </section>
-    </Card>
+    <section className="star">
+      <Card className="Card rounded-lg border-cyan-500 flex flex-col justify-center items-center max-w max-h">
+        <Image
+          src="/profile.jpg"
+          width={150}
+          height={150}
+          alt="profileSebastien"
+          className="rounded-lg mx-auto mt-5 mb-10"
+        />
+        <div className="text-center">
+          <h1 className=" text-white text-6xl pb-4 mt-5 mb-10">
+            Sébastien Wendling
+          </h1>
+          <h3 className="text-white text-3xl mb-10">
+            Développeur web React/WordPress.
+          </h3>
+        </div>
+      </Card>
+    </section>
   );
 };
