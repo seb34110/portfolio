9b65fc5b6612b65f8ea01e660da27bc49a07d164
Sebastien34 <sebastienwendling@iteeweb.fr>
2024-01-24 15:14:38 +0100

    skills

--- src/pages/components/skills.tsx
+++ src/pages/components/skills.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Skills() {
   return (
     <>
-      <section className="">
-        <div className="ml-4 flex-row flex hover:flex-row hover:animate-bounce delay-75">
+      <section className="md:w-1/2">
+        <div className="mx-3 flex-row flex hover:flex-row hover:animate-bounce delay-75">
           <svg
             href="../icons/wordpress"
             data-name="wordpress"
